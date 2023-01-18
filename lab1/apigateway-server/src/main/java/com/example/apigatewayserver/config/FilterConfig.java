323cac7b7aefd473df4a763182b996105f4373e4
조용일 <yedn1128@naver.com>
2023-01-18 18:12:19 +0900

    Spring Cloud Gateway - Filter 적용 2

--- lab1/apigateway-server/src/main/java/com/example/apigatewayserver/config/FilterConfig.java
+++ lab1/apigateway-server/src/main/java/com/example/apigatewayserver/config/FilterConfig.java
@@ -1,2 +1,27 @@
-package com.example.apigatewayserver.config;public class FilterConfig {
+package com.example.apigatewayserver.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class FilterConfig {
+
+//    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // 1. /first-service/** route 등록
+                // 2. filter 적용
+                // 3. http://127.0.0.1:8081 경로로 이동
+                .route(r -> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+                                .addResponseHeader("first-response", "first-response"))
+                        .uri("http://127.0.0.1:8081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-response", "second-response"))
+                        .uri("http://127.0.0.1:8082"))
+                .build();
+    }
 }
