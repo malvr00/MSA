67f41687ac2b51529eb1e328f1fc62b48e7afc20
조용일 <yedn1128@naver.com>
2023-03-08 18:44:39 +0900

    Spring Cloud Gateway 에서 Spring Config 연동 3

--- lab1/apigateway-server/src/main/java/com/example/apigatewayserver/ApigatewayServerApplication.java
+++ lab1/apigateway-server/src/main/java/com/example/apigatewayserver/ApigatewayServerApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigatewayserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServerApplication {
@@ -10,4 +13,8 @@ public class ApigatewayServerApplication {
 		SpringApplication.run(ApigatewayServerApplication.class, args);
 	}
 
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
 }
