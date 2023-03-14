3e9c07a869feba1eca8a701926e9edd2e930579e
조용일 <yedn1128@naver.com>
2023-03-14 17:42:25 +0900

    FeignClient 예외 처리

--- lab1/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ lab1/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,7 +5,9 @@ import com.example.userservice.domain.UserEntity;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.repository.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
@@ -25,6 +27,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
@@ -91,8 +94,14 @@ public class UserServiceImpl implements UserService{
 
         /**
          * Using as feign client
+         * Feign Exception Handling
          */
-        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+        List<ResponseOrder> orderList = null;
+        try {
+            orderServiceClient.getOrders(userId);
+        } catch (FeignException exception) {
+            log.error(exception.getMessage());
+        }
 
         userDto.setOrders(orderList);
 
