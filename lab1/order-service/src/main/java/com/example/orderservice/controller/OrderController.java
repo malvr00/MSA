d7e1ce1b32a64bcf4fcf52a557879e8b342e57a1
조용일 <yedn1128@naver.com>
2023-06-08 19:43:34 +0900

    Apache Kafka의 활용 2 - Orders Microservice 수정 _ Order Kafka Producer

--- lab1/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ lab1/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.orderservice.controller;
 import com.example.orderservice.domain.OrderEntity;
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.messagequeue.OrderProducer;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,26 +30,35 @@ public class OrderController {
 
     private final KafkaProducer kafkaProducer;
 
+    private final OrderProducer orderProducer;
+
     @GetMapping("/health_check")
     public String status() {
         return String.format("It's Working in Order Service on PORT %s", env.getProperty("local.server.port"));
     }
 
     @PostMapping("/{userId}/orders")
-    public ResponseEntity<ResponseOrder> createOrder(@RequestBody RequestOrder order, @PathVariable String userId) {
+    public ResponseEntity<ResponseOrder> createOrder(@RequestBody RequestOrder order, @PathVariable String userId,
+                                                     @RequestBody RequestOrder orderDetails) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
-        /* JPA */
         OrderDto orderDto = modelMapper.map(order, OrderDto.class);
         orderDto.setUserId(userId);
 
-        OrderDto createdOrder = orderService.createOrder(orderDto);
+        /* JPA */
+//        OrderDto createdOrder = orderService.createOrder(orderDto);
+//        ResponseOrder responseUser = modelMapper.map(createdOrder, ResponseOrder.class);
 
-        ResponseOrder responseUser = modelMapper.map(createdOrder, ResponseOrder.class);
+        /* Kafka */
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
 
         /* send this order to the kafka */
         kafkaProducer.send("example-catalog-topic", orderDto);
+        orderProducer.send("orders", orderDto);
+
+        ResponseOrder responseUser = modelMapper.map(orderDto, ResponseOrder.class);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
